427eaf24e4a4392f66ed592d6e1b2751758cccb5
HelenaNotFunny <mh330413@gmail.com>
2023-12-03 15:17:26 -0300

    atualizações?
    i hope it works

--- sketch.js
+++ sketch.js
@@ -1,24 +1,41 @@
+/*Coisas para ainda serem feitas:
+  Transformar os botões em uma função só
+  Transformar a altura e largura do botão de volta em um variável
+  Usar apenas a função mouse clicked
+*/
+
 // Criação de variáveis relacionadas a posição dos botões
 var tela = 1
+
 var largura_botao = 180
 var altura_botao = 70
+
+var largura_tecla = 180
+var altura_tecla = 100
+
+var yMenu = 475
 var xMenu1 = 378
 var xMenu2 = 593 //35 entre os retângulos
 var xMenu3 = 808
-var yMenu = 475
+
 var larguraLetra = 40
+
+var vidas = 5
+
+var Tela_Largura = window.innerWidth, Tela_Altura = window.innerHeight; 
 
 //Carregação de fotos e logos
 function preload() {
   logo = loadImage('assets/puzzle.png');
   fontVT323 = loadFont('assets/VT323-Regular.ttf');
   fontPixelify = loadFont('assets/PixelifySans.ttf');
-  // Letras rosa
   conexao_rosa = loadImage('assets/crosa.png');
   instruçoes = loadImage('assets/instrucoes.png')
   creditos = loadImage('assets/creditos.png')
   eu = loadImage('assets/me.png')
   wyna_liu =loadImage('assets/wynaliu.png')
+  full_heart = loadImage('assets/full_heart2.png')
+  empty_heart = loadImage('assets/empty_heart2.png')
 }
 
 // As cores utilizadas
@@ -26,20 +43,27 @@ var rosa = '#EA2E8F'
 var rosa_escuro = '#990752'
 var rosa_claro = '#F1E4F0'
 
+<<<<<<< Updated upstream
 /*Criação do espaço utilizado - 1366 e 643 são os tamanhos da minha janela*/
+=======
+// Jogo Fácil
+Facil = ['Catapimbas', 'Carambola', 'Cruzes', 'Credo', "Francês", "Cacetinho", "Carioquinha", 'Sal', 'Manga', 'Saia', 'Meia', 'Bota', 'Pão de Açúcar', 'Mermão', 'Samba', 'Dom Pedro II']
+Medio = ['Do', 'Re', 'Mi', 'Fa', ' ']
+Dificil = []
+
+//Criação do espaço utilizado - 1366, 643
+>>>>>>> Stashed changes
 function setup() {
-  createCanvas(1366, 643); 
+  createCanvas(Tela_Largura, Tela_Altura); 
+  Facil_Misturado = shuffle(Facil)
 }
 
 // Botão de como funciona
-function BotaoFuncionamento(){
+function Botao1(numero, texto){
   if(mouseX>xMenu1 && mouseX<xMenu1+largura_botao && mouseY>yMenu && mouseY<yMenu+altura_botao){
     stroke(0)
     fill(rosa_escuro)
     rect(xMenu1, yMenu, largura_botao, altura_botao, 25);
-    if(mouseIsPressed){
-      tela = 2
-    }
   }
   else{
     stroke(0)
@@ -48,18 +72,15 @@ function BotaoFuncionamento(){
   }
   fill(0)
   noStroke()
-  text('Instruções', xMenu1+90, yMenu+45)
+  text(texto, xMenu1+90, yMenu+45)
 }
 
 // Botão de jogar
-function BotaoJogar(){
+function Botao2(numero, texto){
   if(mouseX>xMenu2 && mouseX<xMenu2+largura_botao && mouseY>yMenu && mouseY<yMenu+altura_botao){
     stroke(0)
     fill(rosa_escuro)
     rect(xMenu2, yMenu, largura_botao, altura_botao, 25);
-    if(mouseIsPressed){
-      tela = 3
-    }
   }
   else{
     stroke(0)
@@ -68,18 +89,15 @@ function BotaoJogar(){
   }
   noStroke()
   fill(0)
-  text('Jogar', xMenu2+90, yMenu+45)
+  text(texto, xMenu2+90, yMenu+45)
 }
 
 // Botão de créditos
-function BotaoCreditos(){
+function Botao3(numero, texto){
   if(mouseX>xMenu3 && mouseX<xMenu3+largura_botao && mouseY>yMenu && mouseY<yMenu+altura_botao){
     stroke(0)
     fill(rosa_escuro)
     rect(xMenu3, yMenu, largura_botao, altura_botao, 25);
-    if(mouseIsPressed){
-      tela = 4
-    }
   }
   else{
     stroke(0)
@@ -89,10 +107,10 @@ function BotaoCreditos(){
 
   fill(0)
   noStroke()
-  text('Créditos', xMenu3+90, yMenu+45)
+  text(texto, xMenu3+90, yMenu+45)
 }
 
-// Função da tela de menu
+// Tela de menu
 function TelaMenu(){
   background(rosa_claro); 
     textAlign(CENTER)
@@ -100,21 +118,27 @@ function TelaMenu(){
     image(conexao_rosa, 353.5, 70, 658.66, 168);
 
     strokeWeight(4);
-    BotaoFuncionamento();
-    BotaoJogar();
-    BotaoCreditos();
+    Botao1(2, "Instruções");
+    Botao2(3, "Jogar");
+    Botao3(4, "Créditos");
    
 }
 
-function TelaInstruçoes(){
+// Padrão das telas de instrução e créditos
+function Fundo(){
   noFill()
   background(rosa);
   textSize(35)
-  BotaoVoltar()
+  BotaoVoltar(1)
   fill(rosa_claro)
-  rect(250, 75, 863, 500, 25)
-  image(instruçoes, 451.1, 80, 460.8, 97.2);
   textAlign(LEFT)
+  rect(250, 75, 863, 500, 25)
+}
+
+// Tela de instruções
+function TelaInstruçoes(){
+  Fundo()
+  image(instruçoes, 451.1, 80, 460.8, 97.2);
   fill(0)
   noStroke()
   text('* Ache grupos de 4 itens que tenham uma conexão em comum' +'\n' + 
@@ -123,15 +147,43 @@ function TelaInstruçoes(){
   '* Cada enigma tem apenas uma solução correta!', 300, 225, 750, 300)
 }
 
-// Função da tela de créditos
-function TelaCreditos(){
+// Botões do jogo
+function TeclaJogo(x, y, texto){
+  stroke(0)
+  fill(rosa_claro)
+  rect(x, y, largura_tecla, altura_tecla, 25);
+  fill(0)
+  noStroke()
+  text(texto, x+90, y+58)
+}
+
+// Tela do jogo
+function TelaJogo(){
   noFill()
   background(rosa);
-  textSize(35)
-  BotaoVoltar()
-  textAlign(LEFT)
   fill(rosa_claro)
-  rect(250, 75, 863, 483, 25)
+  BotaoVoltar()
+  textSize(30)
+  contador_linha = 0
+  yTecla = 55
+  // Teclas
+  for(i=0; i<16; i++){
+    TeclaJogo(300.5 + 195*contador_linha, yTecla, Facil_Misturado[i])
+    contador_linha++
+    if(contador_linha > 3){
+      contador_linha = 0
+      yTecla = yTecla + 115
+    }
+  }
+  // Vidas
+  for(i=0; i<vidas; i++){
+    image(full_heart, 583 + 40*i, 515);
+  }
+}
+
+// Tela de créditos
+function TelaCreditos(){
+  Fundo()
   image(creditos, 476.1, 80, 410.8, 97.2);
   image(eu, 300, 200, 65, 65)
   image(wyna_liu, 300, 290, 65, 65)
@@ -141,7 +193,6 @@ function TelaCreditos(){
   text('Wyna Liu - Criadora do jogo original', 400, 330)
 
 }
-
 
 //Criação do botão de voltar
 function BotaoVoltar(){
@@ -154,9 +205,6 @@ function BotaoVoltar(){
   fill(rosa)
   if(mouseX>30 && mouseX<75 && mouseY>30 && mouseY<75){
     fill(rosa_escuro)
-    if(mouseIsPressed){
-      tela = 1
-    }
   }
   stroke(0)
   text('<=', 52, 62)
@@ -166,6 +214,7 @@ function BotaoVoltar(){
 function draw() {
   textFont(fontVT323)
   textAlign(CENTER)
+  strokeWeight(4);
   //Tela de menu
   if(tela == 1){
     TelaMenu();
@@ -174,13 +223,9 @@ function draw() {
   if(tela == 2){
    TelaInstruçoes()
   } 
-  //Tela do JOGO
+  //Tela da escolha de dificuldades
   if(tela == 3){
-    background(rosa);
-    fill(rosa_claro)
-    textSize(35)
-    BotaoVoltar()
-
+    TelaJogo()
   }
   //Tela dos créditos
   if(tela == 4){
@@ -188,4 +233,28 @@ function draw() {
   }
 }
 
+function mouseClicked(){
+  // Botão de voltar
+  if(mouseX>30 && mouseX<75 && mouseY>30 && mouseY<75){
+    if(tela == 2 || tela == 3 || tela == 4){
+      tela = 1
+    } else if(tela == 5){
+      tela = 3
+    }
+  }
+  // Botões de telas
+  if(mouseY>yMenu && mouseY<yMenu+altura_botao){
+    if(tela == 1){
+      if(mouseX>xMenu1 && mouseX<xMenu1+largura_botao){
+        tela = 2
+      }
+      if(mouseX>xMenu2 && mouseX<xMenu2+largura_botao){
+        tela = 3
+      }
+      if(mouseX>xMenu3 && mouseX<xMenu3+largura_botao){
+        tela = 4
+      }
+    }
+  }
+}
 
